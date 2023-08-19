9d7bc270cb39a943423e9ed55974c27d94f5c95f
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-08-20 00:50:01 +0200

    chore: setup project

---
+++ android/app/src/main/kotlin/co/modestnerds/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.modestnerds.car_dash_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:car_dash_2/app/app.dart';
+import 'package:car_dash_2/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
