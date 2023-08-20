59f729fdc00bce424b326c60bf0eb7701b3f06a0
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-08-20 16:17:54 +0200

    feat: weather widget

--- lib/dash/views/widgets/weather_widget.dart
+++ lib/dash/views/widgets/weather_widget.dart
@@ -7,6 +7,7 @@
 //
 
 import 'package:flutter/material.dart';
+import 'package:handy_extensions/handy_extensions.dart';
 import 'package:relative_scale/relative_scale.dart';
 
 import '../../../shared/configs/colors.dart';
@@ -22,9 +23,81 @@ class WeatherWidget extends StatelessWidget {
       builder: (context, height, width, sy, sx) {
         return Container(
           height: sy(120),
+          width: context.width,
           decoration: BoxDecoration(
-            color: DashColors.white,
             borderRadius: BorderRadius.circular(20),
+            image: const DecorationImage(
+              image: AssetImage(
+                'assets/images/cloud.jpg',
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: sx(10),
+              vertical: sy(10),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue.withOpacity(0.3),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Sandton, South Africa',
+                      style: TextStyle(
+                        color: DashColors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: sy(9),
+                      ),
+                    ),
+                    Text(
+                      '24°',
+                      style: TextStyle(
+                        color: DashColors.white,
+                        fontWeight: FontWeight.w700,
+                        fontSize: sy(30),
+                      ),
+                    ),
+                    Text(
+                      'Partly Cloudy',
+                      style: TextStyle(
+                        color: DashColors.white,
+                        fontWeight: FontWeight.w700,
+                        fontSize: sy(9),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Precipitation 5%',
+                      style: TextStyle(
+                        color: DashColors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: sy(9),
+                      ),
+                    ),
+                    Text(
+                      'Humidity 18%',
+                      style: TextStyle(
+                        color: DashColors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: sy(9),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         );
       },
